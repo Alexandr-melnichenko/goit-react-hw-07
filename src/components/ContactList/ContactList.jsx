cdf37fe7ebc1944212f95435f774bb4e1cae0a3d
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2024-12-06 19:14:12 +0200

    updeted hw-7

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,30 +1,18 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import s from "./ContactList.module.css";
-import { selectNameFilter } from "../../redux/filtersSlice";
-import {
-  selectContacts,
-  selectFilteredContacts,
-} from "../../redux/contactsSlice";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 const ContactList = () => {
-  const searchValue = useSelector(selectNameFilter);
-  const contacts = useSelector(selectContacts);
   const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={s.listContact}>
-      {searchValue === ""
-        ? contacts.map((contact) => (
-            <li key={contact.id}>
-              <Contact contact={contact} />
-            </li>
-          ))
-        : filteredContacts.map((contact) => (
-            <li key={contact.id}>
-              <Contact contact={contact} />
-            </li>
-          ))}
+      {filteredContacts.map((contact) => (
+        <li key={contact.id}>
+          <Contact contact={contact} />
+        </li>
+      ))}
     </ul>
   );
 };
